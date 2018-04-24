b04ea1760c0cf0090330bb57784817d60b64c2e2
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-04-24 20:11:07 +0900

    auth 추가

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,7 @@ const user = require('./user');
 const authMiddleware = require('../../middlewares/auth');
 
 router.use('/auth', auth);
+router.use('/user', authMiddleware)
 router.use('/user', user);
 
 module.exports = router;
