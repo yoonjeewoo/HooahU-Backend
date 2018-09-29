70b8520a245c22adc422969210c0507636fae94a
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-09-29 18:27:21 +0900

    facebok

--- routes/api/auth/auth.controller.js
+++ routes/api/auth/auth.controller.js
@@ -8,81 +8,45 @@ const passport = require('passport');
 
 exports.register = (req, res) => {
 	const secret = req.app.get('jwt-secret');
-	const { first_name, last_name, nickname, email, password, type, c_type, w_type, camp, area, reason, facebook_id, is_facebook_login } = req.body;
+	const { first_name, last_name, nickname, email, password, type, c_type, w_type, camp, area, reason } = req.body;
 	const d = new Date();
 	d.setUTCHours(d.getUTCHours());
-	if (is_facebook_login) {
-		conn.query('SELECT * from Users WHERE email=?', [email], (err, rows) => {
-			if (err) throw err;
-			if (rows.length == 0) {
-				conn.query(
-					'INSERT INTO Users(first_name, last_name, nickname, email, password, type, c_type, w_type, camp, area, reason, facebook_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-					[first_name, last_name, nickname, email, password, type, c_type, w_type, camp, area, reason, facebook_id],
-					(err, result) => {
-						if (err) throw err;
-						console.log(result);
-						jwt.sign(
-							{
-								_id: result.insertId,
-								email: email
-							},
-							secret,
-							{
-								// expiresIn: '',
-								issuer: 'rebay_admin',
-								subject: 'userInfo'
-							}, (err, token) => {
-								if (err) return res.status(406).json({ message: 'register failed' });
-								return res.status(200).json({
-									message: 'registered successfully',
-									token
-								});
-							});
-					});
-			} else {
-				return res.status(406).json({
-					message: 'user email or username exists'
-				})
-			}
-		});
-	} else {
-		const encrypted = crypto.createHmac('sha1', config.secret)
+	const encrypted = crypto.createHmac('sha1', config.secret)
 		.update(password)
 		.digest('base64');
-		conn.query('SELECT * from Users WHERE email=?', [email], (err, rows) => {
-			if (err) throw err;
-			if (rows.length == 0) {
-				conn.query(
-					'INSERT INTO Users(first_name, last_name, nickname, email, password, type, c_type, w_type, camp, area, reason) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-					[first_name, last_name, nickname, email, encrypted, type, c_type, w_type, camp, area, reason],
-					(err, result) => {
-						if (err) throw err;
-						console.log(result);
-						jwt.sign(
-							{
-								_id: result.insertId,
-								email: email
-							},
-							secret,
-							{
-								// expiresIn: '',
-								issuer: 'rebay_admin',
-								subject: 'userInfo'
-							}, (err, token) => {
-								if (err) return res.status(406).json({ message: 'register failed' });
-								return res.status(200).json({
-									message: 'registered successfully',
-									token
-								});
+	conn.query('SELECT * from Users WHERE email=?', [email], (err, rows) => {
+		if (err) throw err;
+		if (rows.length == 0) {
+			conn.query(
+				'INSERT INTO Users(first_name, last_name, nickname, email, password, type, c_type, w_type, camp, area, reason) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+				[first_name, last_name, nickname, email, encrypted, type, c_type, w_type, camp, area, reason],
+				(err, result) => {
+					if (err) throw err;
+					console.log(result);
+					jwt.sign(
+						{
+							_id: result.insertId,
+							email: email
+						},
+						secret,
+						{
+							// expiresIn: '',
+							issuer: 'rebay_admin',
+							subject: 'userInfo'
+						}, (err, token) => {
+							if (err) return res.status(406).json({ message: 'register failed' });
+							return res.status(200).json({
+								message: 'registered successfully',
+								token
 							});
-					});
-			} else {
-				return res.status(406).json({
-					message: 'user email or username exists'
-				})
-			}
-		});
-	}
+						});
+				});
+		} else {
+			return res.status(406).json({
+				message: 'user email or username exists'
+			})
+		}
+	});
 };
 
 exports.login = (req, res) => {
